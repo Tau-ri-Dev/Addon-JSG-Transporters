f0d49f58cf7a599a6a2a34357ad43f684d36b014
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-04-19 23:41:03 +1200

    Basic handeling of waterlogged blocks

--- src/main/java/dev/tauri/jsgtransporters/common/helpers/TeleportHelper.java
+++ src/main/java/dev/tauri/jsgtransporters/common/helpers/TeleportHelper.java
@@ -11,8 +11,10 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.FlowingFluid;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
@@ -26,38 +28,54 @@ import static dev.tauri.jsg.stargate.teleportation.TeleportHelper.fireTravelToDi
 import static dev.tauri.jsg.stargate.teleportation.TeleportHelper.setRotationAndPositionAndMotion;
 
 public class TeleportHelper {
-    public static void teleportEntity(Entity entity, RingsPos sourceRings, RingsPos targetRings) {
-        Level world = entity.level();
-        ResourceKey<Level> sourceDim = world.dimension();
+  public static void teleportEntity(Entity entity, RingsPos sourceRings, RingsPos targetRings) {
+    Level world = entity.level();
+    ResourceKey<Level> sourceDim = world.dimension();
 
-        var offset = entity.position().subtract(sourceRings.ringsPos.getCenter().add(0, sourceRings.getBlockEntity().getVerticalOffset(), 0));
-        var tPos = targetRings.ringsPos.getCenter().add(0, targetRings.getBlockEntity().getVerticalOffset(), 0).add(offset);
+    var offset = entity.position()
+        .subtract(sourceRings.ringsPos.getCenter().add(0, sourceRings.getBlockEntity().getVerticalOffset(), 0));
+    var tPos = targetRings.ringsPos.getCenter().add(0, targetRings.getBlockEntity().getVerticalOffset(), 0).add(offset);
 
-        if (sourceDim == targetRings.dimension) {
-            setRotationAndPositionAndMotion(entity, entity.getYHeadRot(), new Vector3d(tPos.x, tPos.y, tPos.z), new Vector3f(), false);
-        } else {
-            if (!fireTravelToDimEvent(entity, targetRings.dimension)) return;
+    if (sourceDim == targetRings.dimension) {
+      setRotationAndPositionAndMotion(entity, entity.getYHeadRot(), new Vector3d(tPos.x, tPos.y, tPos.z),
+          new Vector3f(), false);
+    } else {
+      if (!fireTravelToDimEvent(entity, targetRings.dimension))
+        return;
 
-            entity.changeDimension(Objects.requireNonNull(Objects.requireNonNull(entity.getServer()).getLevel(targetRings.dimension)), new JSGGateTeleporter(new Vector3d(tPos.x, tPos.y, tPos.z), new Vector3f(), entity.getYRot(), null, false, null));
-        }
+      entity.changeDimension(
+          Objects.requireNonNull(Objects.requireNonNull(entity.getServer()).getLevel(targetRings.dimension)),
+          new JSGGateTeleporter(new Vector3d(tPos.x, tPos.y, tPos.z), new Vector3f(), entity.getYRot(), null, false,
+              null));
     }
+  }
 
-    public static BlockState applyStateChanges(BlockState oldState){
-      FluidState fluidState = oldState.getFluidState();
-      if (oldState.getBlock() instanceof LiquidBlock block && fluidState.isSource()){
-        // temp
-        JSG.logger.info(fluidState.is(TagsRegistry.TRANSPORTER_FLUIDS)+" "+fluidState.getFluidType()); // temp
-        Fluid fluid = fluidState.getType();
-        if (fluid instanceof FlowingFluid flowFluid && switch(JSGTConfig.General.ringsFluidTreatmentMode.get()){
+  public static BlockState applyStateChanges(BlockState oldState) {
+    final FluidState waterState = Blocks.WATER.defaultBlockState().getFluidState();
+    FluidState fluidState = oldState.getFluidState();
+    if (oldState.getBlock() instanceof LiquidBlock block && fluidState.isSource()) {
+      // temp
+      JSG.logger.info(fluidState.is(TagsRegistry.TRANSPORTER_FLUIDS) + " " + fluidState.getFluidType()); // temp
+      Fluid fluid = fluidState.getType();
+      if (fluid instanceof FlowingFluid flowFluid && switch (JSGTConfig.General.ringsFluidTreatmentMode.get()) {
+        case Always -> true;
+        case Never -> false;
+        case ByTag -> fluidState.is(TagsRegistry.TRANSPORTER_FLUIDS);
+        case ExcludeTag -> !fluidState.is(TagsRegistry.TRANSPORTER_FLUIDS);
+      }) {
+        FluidState newState = flowFluid.getFlowing().defaultFluidState().setValue(FlowingFluid.LEVEL, 7);
+        return newState.createLegacyBlock();
+      }
+    } else if (oldState.hasProperty(BlockStateProperties.WATERLOGGED) &&
+        oldState.getValue(BlockStateProperties.WATERLOGGED) &&
+        switch (JSGTConfig.General.ringsFluidTreatmentMode.get()) {
           case Always -> true;
           case Never -> false;
-          case ByTag -> fluidState.is(TagsRegistry.TRANSPORTER_FLUIDS);
-          case ExcludeTag -> !fluidState.is(TagsRegistry.TRANSPORTER_FLUIDS);
-        }){
-          FluidState newState = flowFluid.getFlowing().defaultFluidState().setValue(FlowingFluid.LEVEL, 7);
-          return newState.createLegacyBlock();
-        }
-      }
-      return oldState;
+          case ByTag -> waterState.is(TagsRegistry.TRANSPORTER_FLUIDS);
+          case ExcludeTag -> !waterState.is(TagsRegistry.TRANSPORTER_FLUIDS);
+        }) {
+      return oldState.setValue(BlockStateProperties.WATERLOGGED, false);
     }
+    return oldState;
+  }
 }
