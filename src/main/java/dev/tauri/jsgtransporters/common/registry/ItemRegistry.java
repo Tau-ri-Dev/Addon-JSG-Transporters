3b63c90aba0c2cff720d55ed80210a4b8374d1c4
Fredyman95 <93224497+Fredyman95@users.noreply.github.com>
2025-06-15 12:16:20 +0200

    Registered transport ring fragments

--- src/main/java/dev/tauri/jsgtransporters/common/registry/ItemRegistry.java
+++ src/main/java/dev/tauri/jsgtransporters/common/registry/ItemRegistry.java
@@ -28,6 +28,28 @@ public class ItemRegistry {
     public static final RegistryObject<JSGItem> CRYSTAL_UPGRADE_DIM_TUNNELING = Constants.ITEM_HELPER.createGenericItemWithGenericTooltip(
             "crystal_upgrade_dimensional_tunneling", List.of(dev.tauri.jsgtransporters.common.registry.TabRegistry.TAB_RINGS, TabRegistry.TAB_UPGRADES));
 
+    /**
+     * Ring fragments
+     */
+    public static final RegistryObject<JSGItem> FRAGMENT_GOAULD = Constants.ITEM_HELPER.createGenericItem("fragment_rings_goauld", List.of(dev.tauri.jsgtransporters.common.registry.TabRegistry.TAB_RINGS));
+    public static final RegistryObject<JSGItem> FRAGMENT_ORI = Constants.ITEM_HELPER.createGenericItem("fragment_rings_ori", List.of(dev.tauri.jsgtransporters.common.registry.TabRegistry.TAB_RINGS));
+    public static final RegistryObject<JSGItem> FRAGMENT_ANCIENT = Constants.ITEM_HELPER.createGenericItem("fragment_rings_ancient", List.of(dev.tauri.jsgtransporters.common.registry.TabRegistry.TAB_RINGS));
+
+    /**
+     * Crafting elements
+     */
+
+    /**
+
+    prepared registration of new crafting elements
+
+    public static final RegistryObject<JSGItem> GOAULD_BUTTONS = Constants.ITEM_HELPER.createGenericItem("goauld_controller_buttons", List.of(dev.tauri.jsgtransporters.common.registry.TabRegistry.TAB_RINGS));
+    public static final RegistryObject<JSGItem> ORI_BUTTONS = Constants.ITEM_HELPER.createGenericItem("ori_controller_buttons", List.of(dev.tauri.jsgtransporters.common.registry.TabRegistry.TAB_RINGS));
+    public static final RegistryObject<JSGItem> ORI_MAIN_BUTTON = Constants.ITEM_HELPER.createGenericItem("ori_main_controller_button", List.of(dev.tauri.jsgtransporters.common.registry.TabRegistry.TAB_RINGS));
+    public static final RegistryObject<JSGItem> ANCIENT_BUTTONS = Constants.ITEM_HELPER.createGenericItem("ancient_controller_buttons", List.of(dev.tauri.jsgtransporters.common.registry.TabRegistry.TAB_RINGS));
+    public static final RegistryObject<JSGItem> ORI_CONTROLLER_BASE = Constants.ITEM_HELPER.createGenericItem("ori_controller_base", List.of(dev.tauri.jsgtransporters.common.registry.TabRegistry.TAB_RINGS));
+
+    */
 
     public static void register(IEventBus bus) {
         TabHelper.indexItemRegistry(() -> REGISTER);
