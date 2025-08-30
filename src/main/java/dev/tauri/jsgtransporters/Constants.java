29fc26d93634c1706690a591e82fc8e21ec0c44b
MineDragonCZ_ <minedragoncz@email.cz>
2025-08-30 23:27:20 +0200

    [release] Fixed serverside crash

--- src/main/java/dev/tauri/jsgtransporters/Constants.java
+++ src/main/java/dev/tauri/jsgtransporters/Constants.java
@@ -5,12 +5,9 @@ import dev.tauri.jsg.helpers.registry.item.ItemRegistryHelperGeneric;
 import dev.tauri.jsg.loader.LoadersHolder;
 import dev.tauri.jsgtransporters.common.registry.ItemRegistry;
 import dev.tauri.jsgtransporters.common.registry.TabRegistry;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class Constants {
     // contains instances of model and texture loaders for this addon
-    @OnlyIn(Dist.CLIENT)
     public static final LoadersHolder LOADERS_HOLDER = LoadersHolder.getOrCreate(JSGTransporters.MOD_ID, JSGTransporters.class);
 
     // helpers
