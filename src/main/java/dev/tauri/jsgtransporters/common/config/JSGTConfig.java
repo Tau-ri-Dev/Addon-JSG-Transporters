9765f7a7f3e41235288521b60b90b9a9461dfa7a
MineDragonCZ_ <minedragoncz@email.cz>
2025-11-11 13:21:03 +0100

    [release] Updated to latest commit of JSG version

--- src/main/java/dev/tauri/jsgtransporters/common/config/JSGTConfig.java
+++ src/main/java/dev/tauri/jsgtransporters/common/config/JSGTConfig.java
@@ -1,7 +1,12 @@
 package dev.tauri.jsgtransporters.common.config;
 
 import dev.tauri.jsg.config.JSGConfig;
+import dev.tauri.jsg.config.values.JSGConfigValue;
+import dev.tauri.jsg.screen.provider.ConfigScreenClientRegister;
+import dev.tauri.jsgtransporters.JSGTransporters;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
@@ -12,35 +17,23 @@ public class JSGTConfig {
 
     private static final ArrayList<JSGConfig.JSGConfigChild> LIST = new ArrayList<>();
 
-    public static void register() {
-        LIST.clear();
-        LIST.add(new JSGConfig.JSGConfigChild(General.BUILDER, "General"));
-        LIST.add(new JSGConfig.JSGConfigChild(Energy.BUILDER, "Energy"));
-
-        for (JSGConfig.JSGConfigChild child : LIST) {
-            ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, child.builder.build(), CONFIG_FILE_NAME + child.name + ".toml");
-        }
-    }
-
-    public static void load() {
-    }
+    public static final JSGConfig.JSGConfigChild C_GENERAL = new JSGConfig.JSGConfigChild(() -> General.BUILDER, "General", JSGTransporters.MOD_ID);
+    public static final JSGConfig.JSGConfigChild C_ENERGY = new JSGConfig.JSGConfigChild(() -> Energy.BUILDER, "Energy", JSGTransporters.MOD_ID);
 
 
     public static class General {
         private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-        public static final ForgeConfigSpec.IntValue ringsRange = BUILDER
-                .comment(
-                        "Rings range radius in same dimension",
-                        "SIDE: SERVER"
-                ).defineInRange("Rings Horizontal Radius", 64, 5, 2048);
+        public static final JSGConfigValue.IntValue ringsRange = C_GENERAL.add(new JSGConfigValue.IntValue(BUILDER, "Rings Horizontal Radius", 64, 5, 2048,
+                "Rings range radius in same dimension",
+                "SIDE: SERVER"
+        ));
 
-        public static final ForgeConfigSpec.IntValue ringsRangeInterDim = BUILDER
-                .comment(
-                        "Rings range between dimensions.",
-                        "To setup space between dimensions, use the JSG dimensional config",
-                        "SIDE: SERVER"
-                ).defineInRange("Rings Dimension Range", 8, 0, 64);
+        public static final JSGConfigValue.IntValue ringsRangeInterDim = C_GENERAL.add(new JSGConfigValue.IntValue(BUILDER, "Rings Dimension Range", 8, 0, 64,
+                "Rings range between dimensions.",
+                "To setup space between dimensions, use the JSG dimensional config",
+                "SIDE: SERVER"
+        ));
 
         public enum FluidTreatmentModes {
             Always,
@@ -49,28 +42,40 @@ public class JSGTConfig {
             ExcludeTag
         }
 
-        public static final ForgeConfigSpec.EnumValue<FluidTreatmentModes> ringsFluidTreatmentMode = BUILDER
-                .comment(
-                        "When to affect fluids when transporting them",
-                        "SIDE: SERVER",
-                        "\"Always\" always converts source blocks to flowing blocks when transporting them",
-                        "\"Never\" never converts",
-                        "\"ByTag\" only converts fluids contained within the jsg_transporters:transporter_fluids tag",
-                        "\"ExcludeTag\" (Default) converts all fluids except those within the tag"
-                ).defineEnum("Rings fluid treatment mode", FluidTreatmentModes.ExcludeTag);
+        public static final JSGConfigValue.EnumValue<FluidTreatmentModes> ringsFluidTreatmentMode = C_GENERAL.add(new JSGConfigValue.EnumValue<>(BUILDER, "Rings fluid treatment mode", FluidTreatmentModes.ExcludeTag,
+                "When to affect fluids when transporting them",
+                "SIDE: SERVER",
+                "\"Always\" always converts source blocks to flowing blocks when transporting them",
+                "\"Never\" never converts",
+                "\"ByTag\" only converts fluids contained within the jsg_transporters:transporter_fluids tag",
+                "\"ExcludeTag\" (Default) converts all fluids except those within the tag"
+        ));
     }
 
     public static class Energy {
         private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-        public static final ForgeConfigSpec.IntValue ringsStartEnergy = BUILDER
-                .comment(
-                        "SIDE: SERVER"
-                ).defineInRange("Rings start power draw", 2048, 0, 500000);
+        public static final JSGConfigValue.IntValue ringsStartEnergy = C_ENERGY.add(new JSGConfigValue.IntValue(BUILDER, "Rings start power draw", 2048, 0, 500000,
+                "SIDE: SERVER"
+        ));
 
-        public static final ForgeConfigSpec.IntValue ringsTransportEnergy = BUILDER
-                .comment(
-                        "SIDE: SERVER"
-                ).defineInRange("Rings entity/block transport power draw", 256, 0, 500000);
+        public static final JSGConfigValue.IntValue ringsTransportEnergy = C_ENERGY.add(new JSGConfigValue.IntValue(BUILDER, "Rings entity/block transport power draw", 56, 0, 500000,
+                "SIDE: SERVER"
+        ));
+    }
+
+    public static void register() {
+        LIST.clear();
+        LIST.add(C_GENERAL);
+        LIST.add(C_ENERGY);
+
+        for (JSGConfig.JSGConfigChild child : LIST) {
+            ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, child.builder.get().build(), CONFIG_FILE_NAME + child.name + ".toml");
+        }
+
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () -> ConfigScreenClientRegister.register(JSGTransporters.MOD_ID, LIST));
+    }
+
+    public static void load() {
     }
 }
